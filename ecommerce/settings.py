7a2dd2bb21b8e1aa455110c98644f7f2b58dae68
Helder Vieira <heldervieiraster@gmail.com>
2019-01-26 19:55:59 +0000

    update

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -101,23 +101,10 @@ COOKIE_CONSENT_CACHE_BACKEND = {
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-"""
-if "DATABASE_URL" in os.environ:
-	DATABASES = {
-	'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
-"""
 
 if "DATABASE_URL" in os.environ:
 	DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'garage-sale',
-        'USER': 'ubuntu',
-        'PASSWORD':'pr0v1da',
-        'HOST':'localhost',
-        'PORT':'5432',
-    }
+	'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 else:
 	print("Postgres URL not found, using sqlite instead")
@@ -131,7 +118,6 @@ else:
         },
 		}
 	}
-
 
 
 
